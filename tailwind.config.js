028de84d66816ab72c9c183881d8c7317688209a
venmario <glacius2ndmioogg@gmail.com>
2024-01-03 16:54:03 +0700

    feat: example custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          "blue": "#3E7BFA",
+          "dark-blue": "#3040DC"
+        }
+      }
+    }
   },
   plugins: []
 };
