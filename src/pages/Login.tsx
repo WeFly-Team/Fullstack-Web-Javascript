efa4cbb72cb4e6fef497b050c70bc0392ab8fce9
ahmadseloabadi <ahmadseloabadi@gmail.com>
2024-01-07 15:32:42 +0700

    feat:add a logo to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,6 +20,13 @@ const Login = () => {
     <>
       <div className="flex items-center h-screen justify-center">
         <div className="form p-7 rounded-xl md:w-2/5 flex flex-col justify-center items-center sm:w-full">
+          <div className="absolute top-2 left-2">
+            <img
+              src="https://i.ibb.co/pxQ7DPC/logo-We-Fly.png"
+              alt="logo"
+              className="w-16"
+            />
+          </div>
           <Heading children="Welcome Back" />
           <FormInput
             children="Email"
