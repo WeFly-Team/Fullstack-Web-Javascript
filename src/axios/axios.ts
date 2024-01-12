c0af4fca3e04db46c9f7a67a6552546c0d97164c
venmario <glacius2ndmioogg@gmail.com>
2024-01-12 14:47:55 +0700

    feat: consume login api

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  import.meta.env['VITE_BACKEND_URL'] || 'http://188.166.196.8:8081/api';
+  import.meta.env['VITE_BACKEND_URL'] || 'http://188.166.196.8:8081/api/v1';
 const axiosInstance = axios.create({
   baseURL,
 });
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,8 +5,15 @@ import Heading from '../../components/Heading';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { SubmitHandler, Controller, useForm } from 'react-hook-form';
 import { IFormInput } from './types';
+import { useAuth } from '../../customHooks/useAuth/useAuth';
+import axiosInstance from '../../axios/axios';
+import { useState } from 'react';
 
 const Login = () => {
+  // useState
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  // useAuth
+  const { login } = useAuth();
   // hook form
   const {
     control,
@@ -18,8 +25,21 @@ const Login = () => {
       password: '',
     },
   });
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     console.log(data);
+    try {
+      const result = await axiosInstance.post('/user-login/login', data);
+      if (result.data.code == 200) {
+        const token = result.data.access_token;
+        login(token);
+      } else if (result.data.code == 400) {
+        if (result.data.error === 'wrong password') {
+          setErrorMessage("User not found or password didn't match");
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -34,6 +54,30 @@ const Login = () => {
             />
           </div>
           <Heading children="Welcome Back" />
+          {errorMessage && (
+            <div
+              className="bg-red-100 border border-red-400 text-red-700 pl-4 pr-8 sm:pr-4 py-3 rounded relative w-full mb-2"
+              role="alert"
+            >
+              <span className="block sm:inline">
+                User not found or password didn't match
+              </span>
+              <span
+                className="absolute top-0 bottom-0 right-0 px-4 py-3"
+                onClick={() => !errorMessage}
+              >
+                <svg
+                  className="fill-current h-6 w-6 text-red-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </span>
+            </div>
+          )}
           <form onSubmit={handleSubmit(onSubmit)} className="w-full">
             <Controller
               name="email"
