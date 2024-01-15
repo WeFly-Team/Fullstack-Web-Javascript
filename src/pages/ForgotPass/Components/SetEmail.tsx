728d589105f901f7b6e9a9be67a3a7fd3fb9f5f2
venmario <glacius2ndmioogg@gmail.com>
2024-01-15 13:14:09 +0700

    refactor: set email

--- src/pages/ForgotPass/Components/SetEmail.tsx
+++ src/pages/ForgotPass/Components/SetEmail.tsx
@@ -2,19 +2,22 @@ import { Link } from 'react-router-dom';
 import FormInput from '../../../components/FormInput';
 import Heading from '../../../components/Heading';
 import Button from '../../../components/Button';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import {
   FrogotPassContext,
   IFormInput,
   forgotPasswordContextType,
 } from '../types';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import axiosInstance from '../../../axios/axios';
+import { AxiosError } from 'axios';
 
 const SetEmail = () => {
   const { handleEmail, handleComponent } = useContext(
     FrogotPassContext
   ) as forgotPasswordContextType;
-
+  // useState
+  const [errorMessage, setErrorMessage] = useState<string>('');
   // useForm
   const {
     handleSubmit,
@@ -25,9 +28,23 @@ const SetEmail = () => {
       email: '',
     },
   });
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    handleEmail(data.email);
-    handleComponent('otp');
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    try {
+      const result = await axiosInstance.post(
+        '/forget-password/forgot-password',
+        data
+      );
+      if (result.data.code == 200 && result.data.message === 'success') {
+        handleEmail(data.email);
+        handleComponent('otp');
+      }
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setErrorMessage(err.message);
+      } else if (err instanceof Error) {
+        setErrorMessage(err.message);
+      }
+    }
   };
 
   return (
@@ -43,6 +60,28 @@ const SetEmail = () => {
       <h2 className="text-center text-gray-500 text-sm mb-5">
         No worries, we'll send you reset instructions.
       </h2>
+      {errorMessage && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 pl-4 pr-8 sm:pr-4 py-3 rounded relative w-[300px] mb-2"
+          role="alert"
+        >
+          <span className="block sm:inline">{errorMessage}</span>
+          <span
+            className="absolute top-0 bottom-0 right-0 px-4 py-3"
+            onClick={() => setErrorMessage('')}
+          >
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
       <Controller
         name="email"
         control={control}
