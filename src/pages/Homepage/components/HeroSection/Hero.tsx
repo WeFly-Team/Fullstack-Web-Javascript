bde74b403852bc62b25a9cdac2aa4f4c495f7eea
Febripram <febripramana0@gmail.com>
2024-01-20 02:25:18 +0700

    responsive hero section

--- src/pages/Homepage/components/HeroSection/Hero.tsx
+++ src/pages/Homepage/components/HeroSection/Hero.tsx
@@ -10,46 +10,47 @@ const HeroSection = () => {
         backgroundImage: 'url(https://i.ibb.co/NL7CL8M/image-13.png)',
         bckgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
+        // backgroundAttachment: 'local',
         backgroundPosition: 'center',
         width: 'full',
-        height: '720px'
+        height: 'h-3/6 lg:673px'
     }
 
     return (
-      <section id="hero" style={style} className=''>
-        <div  className='grid lg:grid-cols-8 xl:container'>
-            <div className='text-white pl-16 pt-20 col-span-3'>
-                <div className='pb-4 flex '>
-                    <div className="text-3xl font-semibold pr-6">01</div>
+      <section id="hero" style={style} className='md:position-relative'>
+        <div  className='grid grid-cols-3 md:grid-cols-8 xl:container '>
+            <div className='text-white w-full sm:h-3/6 pt-10 pb-4 lg:pl-16 pl-4 pr-4 lg:mt-12 md:mt-10  col-span-3'>
+                <div className='md:pb-4 pb-2 flex '>
+                    <div className="lg:text-3xl md:text-base text-xs font-semibold lg:pr-6 pr-2">01</div>
                     <div className="">
-                        <h2 className='text-3xl pb-3 font-semibold'>Find Your Dream Destination</h2>
-                        <h4 className='text-xl'>Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
+                        <h2 className='lg:text-3xl md:text-base text-xs lg:pb-3 pb-1 font-semibold'>Find Your Dream Destination</h2>
+                        <h4 className='lg:text-xl md:text-base text-xs '>Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
                     </div>
                 </div>
-                <div className='pb-4 flex'>
-                    <div className="text-3xl font-semibold pr-6">02</div>
+                <div className='md:pb-4 pb-2 flex'>
+                    <div className="lg:text-3xl md:text-base text-xs font-semibold lg:pr-6 pr-2">02</div>
                     <div>
-                        <h2 className='text-3xl pb-3 font-semibold'>Book Tickets Easily</h2>
-                        <h4 className='text-xl'>Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
+                        <h2 className='lg:text-3xl md:text-base text-xs lg:pb-3 pb-1 font-semibold'>Book Tickets Easily</h2>
+                        <h4 className='lg:text-xl md:text-base text-xs '>Eu sceler sque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
                     </div>
                 </div>
-                <div className='pb-4 flex '>
-                    <div className='text-3xl font-semibold pr-6'>03</div>
+                <div className='md:pb-4 pb-2 flex '>
+                    <div className='lg:text-3xl md:text-base text-xs font-semibold lg:pr-6 pr-2'>03</div>
                     <div>
-                        <h2 className='text-3xl pb-3 font-semibold'>Real-Time Flight Monitoring</h2>
-                        <h4 className='text-xl'>Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
+                        <h2 className='lg:text-3xl md:text-base text-xs lg:pb-3 pb-1 font-semibold'>Real-Time Flight Monitoring</h2>
+                        <h4 className='lg:text-xl md:text-base text-xs'>Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.</h4>
                     </div>
                 </div>
             </div>
-            <div className='pt-28 flex justify-end col-span-2'>
-        <img src="https://i.ibb.co/HTv63dH/Whats-App-Image-2024-01-14-at-12-25-1.png"  alt="" />
+            <div className='md:position-fixed md:pt-24  pt-10 sm:pt-2 flex md:justify-center justify-end lg:justify-end  h-full sm:h-full md:h-5/6 lg:h-full col-span-1 sm:col-span-1 md:col-span-2'>
+        <img src="https://i.ibb.co/HTv63dH/Whats-App-Image-2024-01-14-at-12-25-1.png" className='h-3/6 sm:h-5/6 md:h-5/6 lg:h-full'  alt="" />
             </div>
-            <div className='pt-20 w-full pr-20 col-span-3'>
-                <div className='pb-5'>
-                    <h1 className='font-semibold text-white text-5xl text-right'>Stay updated with travel tips, recommendations, and latest promos.</h1>
+            <div className='w-full pr-5 pt-10 md:pt-20 md:pt-28 lg:pt-20 md:pr-10 lg:pr-20 col-span-2 sm:col-span-2  md:col-span-3'>
+                <div className='md:pb-5 pb-2 '>
+                    <h1 className='font-semibold w-full text-white lg:text-5xl md:text-2xl text-sm text-right'>Stay updated with travel tips, recommendations, and latest promos.</h1>
                 </div>
-                <div className='flex justify-end'>
-                    <Button variant="primary" size="md" id="Download">
+                <div className='flex justify-end pl-10'>
+                    <Button variant="primary" size="sm" id="Download">
                     Download WeFly App
                     </Button>
                 </div>
