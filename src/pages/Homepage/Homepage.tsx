0623429aa860fbcdcfc9d730aaf46f7ee33e4cd9
venmario <glacius2ndmioogg@gmail.com>
2024-01-19 17:14:39 +0700

    feat: add footer

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,4 +1,5 @@
 import FindTicket from './components/FindTicket/FindTicket';
+import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
 const Homepage = () => {
@@ -6,6 +7,7 @@ const Homepage = () => {
     <>
       <Navbar className="fixed top-0 right-0 left-0 z-10" />
       <FindTicket />
+      <Footer />
     </>
   );
 };
