277c2c330f006ae0213ea8a7b228fb66bfbcd58d
venmario <glacius2ndmioogg@gmail.com>
2024-01-20 09:57:13 +0700

    fix: add data class

---
+++ src/pages/Homepage/components/FindTicket/data.ts
@@ -0,0 +1,8 @@
+import { classProp } from './types';
+
+export const classOptions: classProp[] = [
+  { label: 'Economy', value: 'Economy' },
+  { label: 'Premium Economy', value: 'PremiumEconomy' },
+  { label: 'Business', value: 'Business' },
+  { label: 'First Class', value: 'FirstClass' },
+];
