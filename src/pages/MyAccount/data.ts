0d5147975893a5597e0ee3097a0d620ad0e6777c
Febripram <febripramana0@gmail.com>
2024-01-24 22:14:44 +0700

    add my account component

---
+++ src/pages/MyAccount/data.ts
@@ -0,0 +1,28 @@
+import { optionsProp } from './types';
+
+// vars
+const today = new Date();
+export const yearsOption: optionsProp[] = [];
+for (let i = today.getFullYear(); i >= 1950; i--) {
+  yearsOption.push({ value: i.toString(), label: i.toString() });
+}
+export const monthsOption: optionsProp[] = [
+  { label: 'January', value: 'January' },
+  { label: 'February', value: 'February' },
+  { label: 'March', value: 'March' },
+  { label: 'April', value: 'April' },
+  { label: 'May', value: 'May' },
+  { label: 'June', value: 'June' },
+  { label: 'July', value: 'July' },
+  { label: 'August', value: 'August' },
+  { label: 'September', value: 'September' },
+  { label: 'October', value: 'October' },
+  { label: 'November', value: 'November' },
+  { label: 'December', value: 'December' },
+];
+export const datesOption: optionsProp[] = [];
+
+export const genderOption: optionsProp[] = [
+  {label: 'Female', value: 'Female'},
+  {label: 'Male', value : 'Male'},
+];
