613f7b77939a3c97a962a4c1e8a6a3413d0c84f4
venmario <glacius2ndmioogg@gmail.com>
2024-01-25 23:36:45 +0700

    refactor: history component

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,19 +1,48 @@
+import { useContext, useState } from 'react';
 import NoResultCard from '../../components/NoResultCard/NoResultCard';
 import BookingCard from '../MyBooking/Component/BookingCard';
+import {
+  UserTransactionContext,
+  userTransactionContextType,
+} from '../ProfileLayout/types';
 
 const HistoryList = () => {
+  const { transactions } = useContext(
+    UserTransactionContext
+  ) as userTransactionContextType;
+
+  const [finishedTransaction, setFinishedTransaction] = useState<number>(0);
   return (
     <div className="">
       <h1 className="font-bold text-2xl">Purchase History</h1>
-      <NoResultCard
-        className="mt-4"
-        title="No History Found"
-        content="You haven't made any purchases after you made your account"
-      />
+      {finishedTransaction == 0 && (
+        <NoResultCard
+          className="mt-4"
+          title="No History Found"
+          content="You haven't made any purchases after you made your account"
+        />
+      )}
 
-      <BookingCard status="finish" className="mt-4" />
-      <BookingCard status="finish" className="mt-4" />
-      <BookingCard status="finish" className="mt-4" />
+      {transactions.length != 0 &&
+        transactions.map((transaction) => {
+          if (transaction.status === 'FINISH') {
+            setFinishedTransaction(finishedTransaction + 1);
+            return (
+              <BookingCard
+                key={transaction.id}
+                status={transaction.status}
+                bookingId={transaction.id}
+                departureAirport={
+                  transaction.transactionDetails[0].flight.departureAirport
+                }
+                arrivalAirport={
+                  transaction.transactionDetails[0].flight.arrivalAirport
+                }
+                className="mt-4"
+              />
+            );
+          }
+        })}
     </div>
   );
 };
