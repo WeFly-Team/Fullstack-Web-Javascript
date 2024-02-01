7f78b1270b3148d2b1e0ffa11501b43a3d3cb690
Febripram <febripramana0@gmail.com>
2024-02-01 16:10:02 +0700

    add feat Destination

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,12 +2,14 @@ import FindTicket from './components/FindTicket/FindTicket';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/Hero';
+import Destination from './components/Destination/Destination';
 
 const Homepage = () => {
   return (
     <>
       <Navbar className="fixed top-0 right-0 left-0 z-10" />
       <FindTicket />
+      <Destination />
       <HeroSection />
       <Footer />
     </>
