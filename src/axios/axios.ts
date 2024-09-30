b8a47b69de5a39069f24aec4f4d2fb9e4a154a15
venmario <glacius2ndmioogg@gmail.com>
2024-09-30 14:47:48 +0700

    chore: update backend url

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const baseURL =
-  import.meta.env['VITE_BACKEND_URL'] || 'https://staging.wefly.my.id/api/v1';
+const baseURL = import.meta.env['VITE_BACKEND_URL'];
 const axiosInstance = axios.create({
   baseURL,
 });
